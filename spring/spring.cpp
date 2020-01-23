e79decf1583fb0a7cb9338c3781d4d2c6d329828
dainuofei <nuofeidai@gmail.com>
2020-01-23 14:57:10 +0800

    Update spring

--- spring/spring.cpp
+++ spring/spring.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int result = 0;
-int n = 0;
-int a[100001];
-void Spring(int pos) {
-    result ++;
-    if (pos + a[pos] > n - 1) {
-        return;
-    } else {
-        Spring(pos + a[pos]);
+int a[100001], dp[100001];
+int main()
+{
+    // freopen("spring.in", "r", stdin);
+    // freopen("spring.out", "w", stdout);
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
+    for (int i = n; i >= 1; --i)
+    {
+        if (i + a[i] > n)
+            dp[i] = 1;
     }
-}
-int main(int argc, char const *argv[]) {
-    memset(a, 0, sizeof(a));
-    scanf("%d", &n);
-    
-    for (int i = 0; i < n; i ++) {
-        scanf("%d", &a[i]);
+    int result = 1;
+    for (int i = n; i >= 1; --i)
+    {
+        int m = i - 30 > 0 ? i - 30 : 0;//保证m不是负数并且可以m = i - 30
+        for (int j = i - 1; j >= m; --j)
+        {
+            if (j + a[j] == i)
+            {
+                dp[j] = dp[i] + 1;
+                result = max(dp[j], result);//找出最大值
+            }
+        }
     }
-    Spring(0);
-    printf("%d", result);
-    system("pause");
+    printf("%d\n", result);
+    // system("pause");
     return 0;
 }
